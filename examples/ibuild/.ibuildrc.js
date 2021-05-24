15a10460c14fa0dc9fe3b0b0d98b6303f9d0c095
kawhi66 <kawhi_site@163.com>
2021-05-24 18:09:29 +0800

    internal: update ibuild example

--- examples/ibuild/.ibuildrc.js
+++ examples/ibuild/.ibuildrc.js
@@ -1,14 +1,24 @@
-export default {
-  file: "test-ibuild",
-  // type: ["cjs", "esm", "umd"],
-  type: "esm",
-  transform: true,
-  transformType: "cjs",
-  disableTypeCheck: false,
-  target: "browser",
-  runtimeHelpers: false,
-  lazy: true,
-  umd: {
-    name: "abc",
+export default [
+  {
+    file: "test-ibuild",
+    // type: ["cjs", "esm", "umd"],
+    type: "esm",
+    transform: true,
+    transformType: "cjs",
+    disableTypeCheck: false,
+    target: "browser",
+    runtimeHelpers: false,
+    lazy: true,
+    umd: {
+      name: "abc",
+    },
   },
-};
+  {
+    entry: "src/utils.js",
+    file: "test-utils",
+    type: "esm",
+    umd: {
+      name: "abcd",
+    },
+  },
+];

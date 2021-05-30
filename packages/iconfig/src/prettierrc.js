21dcbf60856f9a5f98147a563dc70b490f26ea55
kawhi66 <kawhi_site@163.com>
2021-05-30 16:05:57 +0800

    feat:iconfig prettierrc

--- packages/iconfig/src/prettierrc.js
+++ packages/iconfig/src/prettierrc.js
@@ -1,3 +1,12 @@
+// https://prettier.io/docs/en/options.html
 export default {
   singleQuote: true,
+  trailingComma: "all",
+  printWidth: 100,
+  proseWrap: "never",
+  tabWidth: 2,
+  endOfLine: "lf",
+  overrides: [
+    // https://prettier.io/docs/en/configuration.html#configuration-overrides
+  ],
 };

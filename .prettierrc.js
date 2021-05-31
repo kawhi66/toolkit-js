2641e682cf123e18b009572aaa455da783350e39
kawhi66 <kawhi_site@163.com>
2021-05-31 11:09:05 +0800

    internal: prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  proseWrap: 'never',
+  tabWidth: 2,
+  endOfLine: 'lf',
+  overrides: [
+    // https://prettier.io/docs/en/configuration.html#configuration-overrides
+  ],
+};

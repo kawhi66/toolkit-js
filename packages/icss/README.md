18eed1a8a6f3a401a899c187352893d5b76d1263
kawhi66 <kawhi_site@163.com>
2021-06-15 21:08:26 +0800

    feat:icss: init

---
+++ packages/icss/README.md
@@ -0,0 +1,3 @@
+# icss
+
+TODO
---
+++ packages/icss/bin/icss.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+const { existsSync } = require('fs');
+const { join } = require('path');
+const yParser = require('yargs-parser');
+const chalk = require('chalk');
+const signale = require('signale');
+
+// print version and @local
+const args = yParser(process.argv.slice(2));
+if (args.v || args.version) {
+  console.log(require('../package').version);
+  if (existsSync(join(__dirname, '../.local'))) {
+    console.log(chalk.cyan('@local'));
+  }
+  process.exit(0);
+}
+
+// print help
+if (args.h || args.help) {
+  printHelp();
+  process.exit(0);
+}
+
+// Notify update when process exits
+const updater = require('update-notifier');
+const pkg = require('../package.json');
+updater({ pkg }).notify({ defer: true });
+
+// require('../lib')
+//   .default({
+//     watch: args.w || args.watch || false,
+//   })
+//   .catch((e) => {
+//     signale.error(e);
+//     process.exit(1);
+//   });
+
+function printHelp() {
+  console.log(`
+  Usage: icss [options]
+
+  Options:
+
+    ${chalk.green('-w, --watch')}         Watch building (default: false)
+    ${chalk.green('-h, --help')}          Print help
+    ${chalk.green('-v, --version')}       Print version
+  `);
+}

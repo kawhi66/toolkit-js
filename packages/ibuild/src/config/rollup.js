ce8f90dfd12d5f2487fd5e949fd47289ada78bb4
kawhi66 <kawhi_site@163.com>
2021-07-26 15:37:06 +0800

    support vue sfc

--- packages/ibuild/src/config/rollup.js
+++ packages/ibuild/src/config/rollup.js
@@ -15,6 +15,7 @@ import tempDir from 'temp-dir';
 import autoprefixer from 'autoprefixer';
 import NpmImport from 'less-plugin-npm-import';
 import svgr from '@svgr/rollup';
+import vue from 'rollup-plugin-vue';
 import getBabelConfig from './babel';
 
 export default function (opts) {
@@ -87,6 +88,7 @@ export default function (opts) {
   // ref: https://rollupjs.org/guide/en#external
   // 潜在问题：引用包的子文件时会报 warning，比如 @babel/runtime/helpers/esm/createClass
   // 解决方案：可以用 function 处理
+  extraExternals.push('vue');
   const external = [
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
@@ -94,6 +96,14 @@ export default function (opts) {
   ];
   // umd 只要 external peerDependencies
   const externalPeerDeps = [...Object.keys(pkg.peerDependencies || {}), ...extraExternals];
+
+  // UMD shared settings: output.globals
+  // Refer to https://rollupjs.org/guide/en#output-globals for details
+  const globals = {
+    // Provide global variable names to replace your external imports
+    // eg. jquery: '$'
+    vue: 'Vue',
+  };
 
   function getPkgNameByid(id) {
     const splitted = id.split('/');
@@ -123,33 +133,36 @@ export default function (opts) {
 
   function getPlugins(opts = {}) {
     const { minCSS, isUMD } = opts;
+    const postcssPlugins = [
+      autoprefixer({
+        // https://github.com/postcss/autoprefixer/issues/776
+        remove: false,
+        ...autoprefixerOpts,
+      }),
+      ...extraPostCSSPlugins,
+    ];
+    const postcssConfigList = {
+      extract: extractCSS,
+      inject: injectCSS,
+      modules,
+      // modules => all .less will convert into css modules
+      ...(modules ? { autoModules: false } : {}),
+      minimize: !!minCSS,
+      use: {
+        less: {
+          plugins: [new NpmImport({ prefix: '~' })],
+          javascriptEnabled: true,
+        },
+      },
+      plugins: postcssPlugins,
+    };
     return [
+      vue({
+        css: extractCSS,
+      }),
       url(),
       svgr(),
-      postcss({
-        extract: extractCSS,
-        inject: injectCSS,
-        modules,
-        // modules => all .less will convert into css modules
-        ...(modules ? { autoModules: false } : {}),
-        minimize: !!minCSS,
-        use: {
-          less: {
-            plugins: [new NpmImport({ prefix: '~' })],
-            javascriptEnabled: true,
-          },
-          sass: {},
-          stylus: false,
-        },
-        plugins: [
-          autoprefixer({
-            // https://github.com/postcss/autoprefixer/issues/776
-            remove: false,
-            ...autoprefixerOpts,
-          }),
-          ...extraPostCSSPlugins,
-        ],
-      }),
+      postcss(postcssConfigList),
       ...(injectOpts ? [inject(injectOpts)] : []),
       ...(replaceOpts && !isEmpty(replaceOpts)
         ? [replace({ preventAssignment: true, ...replaceOpts })]
@@ -232,7 +245,7 @@ export default function (opts) {
             format,
             sourcemap: umd && umd.sourcemap,
             file: join(cwd, `dist/${name}.umd.js`),
-            globals: umd && umd.globals,
+            globals: umd && { ...umd.globals, ...globals },
             name: (umd && umd.name) || (pkg.name && camelCase(basename(pkg.name))),
             exports: 'named',
           },
@@ -253,7 +266,7 @@ export default function (opts) {
             format,
             sourcemap: umd && umd.sourcemap,
             file: join(cwd, `dist/${name}.umd.min.js`),
-            globals: umd && umd.globals,
+            globals: umd && { ...umd.globals, ...globals },
             name: (umd && umd.name) || (pkg.name && camelCase(basename(pkg.name))),
             exports: 'named',
           },

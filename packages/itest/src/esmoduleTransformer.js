f71d19d3d8dde853bf91ae312e6ac6207806f3b5
kawhi66 <kawhi_site@163.com>
2022-05-16 14:36:28 +0800

    feat: update itest esmoduleTransformer

--- packages/itest/src/esmoduleTransformer.js
+++ packages/itest/src/esmoduleTransformer.js
@@ -1,7 +1,18 @@
 import babelJest from 'babel-jest';
 
 module.exports = babelJest.createTransformer({
-  plugins: ['@babel/plugin-transform-modules-commonjs'],
+  plugins: [
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-do-expressions',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+  ],
   babelrc: false,
   configFile: false,
 });
